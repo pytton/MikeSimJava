e00d0c354abb472d86d7967f39198f8e806e39de
pytton <pytton@hotmail.com>
2019-02-05 12:46:17 -0600

    Initial commit with gradle configuration and basic java fx setup

---
+++ src/main/java/game/AppRunner.java
@@ -0,0 +1,16 @@
+package game;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppRunner extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
