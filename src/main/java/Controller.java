eb6f33d2d0cb1c7ee7427a617326c2fc888c6164
pytton <pytton@hotmail.com>
2019-02-08 19:27:11 -0600

    added PositionsWindow.fxml. builds and runs

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package main.java;
 
 public class Controller {
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package main.java;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,18 +10,21 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("PriceControlPanel.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../resources/SceneBuilder/PriceControlPanel.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setX(1200);
+        primaryStage.setY(50);
 
-
-        Parent root2 = FXMLLoader.load(getClass().getResource("PriceControlPanel.fxml"));
+        Parent root2 = FXMLLoader.load(getClass().getResource("../resources/SceneBuilder/PositionsWindow.fxml"));
 
         Stage secondStage = new Stage();
-        secondStage.setX(300);
+        secondStage.setX(0);
+        secondStage.setY(50);
         secondStage.setScene(new Scene(root2));
+
         secondStage.show();
+        primaryStage.show();
     }
 
 
